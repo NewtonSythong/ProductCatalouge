ce4346f93482bfd2bdcaa9b45fc59cfd84262afd
sytne506 <sytne506@student.otago.ac.nz>
2024-10-02 17:53:54 +1300

    Created cart js. closes #6

---
+++ static/js/cart.js
@@ -0,0 +1,74 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/javascript.js to edit this template
+ */
+
+
+"use strict";
+
+class SaleItem {
+    constructor(product, quantityPurchased) {
+        this.product = product;
+        this.quantityPurchased = quantityPurchased;
+        this.salePrice = product.listPrice;
+    }
+}
+
+class Sale {
+    constructor(customer, items) {
+        this.customer = customer;
+        this.items = items;
+    }
+}
+
+var salesApi = '/api/sales';
+var total = 0;
+
+const app = Vue.createApp({
+
+    data() {
+        return {
+            // models (comma separated key/value pairs)
+
+        };
+    },
+
+    computed: Vuex.mapState({
+        product: 'selectedProduct',
+        items: 'items',
+        customer: 'customer'
+    }),
+
+    mounted() {
+        // semicolon separated statements
+
+
+    },
+
+    methods: {
+        // comma separated function declarations
+        checkOut() {
+            let sale = new Sale(this.customer, this.items);
+            console.log();
+            axios.post(salesApi, sale)
+                    .then(() => {
+                        dataStore.commit("clearItems");
+                        window.location = 'order-confirmation.html';
+                    })
+                    .catch(error => {
+                        alert(error.response.data.message);
+                    });
+        },
+
+    }
+
+});
+
+/* other component imports go here */
+
+// import data store
+import { sessionStore } from './session-store.js'
+        app.use(sessionStore);
+
+// mount the page - this needs to be the last line in the file
+app.mount("main");
