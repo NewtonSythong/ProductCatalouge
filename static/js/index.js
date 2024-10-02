a55fbe33c2997ba592a1aeb4a3f723e368901e0f
sytne506 <sytne506@student.otago.ac.nz>
2024-10-02 16:59:40 +1300

    Added index js. closes #3

---
+++ static/js/index.js
@@ -0,0 +1,20 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/javascript.js to edit this template
+ */
+
+// create the Vue controller
+const app = Vue.createApp();
+
+// import the navigation menu
+import { navigationMenu } from './navigation-menu.js';
+
+// register the navigation menu under the <navmenu> tag
+app.component('navmenu', navigationMenu);
+
+// import session store
+import { sessionStore } from './session-store.js';
+app.use(sessionStore);
+
+// attach the controller to the <main> tag
+app.mount("main");
